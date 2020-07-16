c94e1c72ebbe17b724873f2017f7d95bbe9ba6dd
berquerant <sin@sin-pro.local>
2020-07-16 23:47:59 +0900

    Add filter executor

--- executor.go
+++ executor.go
@@ -40,3 +40,41 @@ func (s *mapExecutor) Execute() (*Iterator, error) {
 	}
 	return NewIterator(f)
 }
+
+type (
+	filterExecutor struct {
+		f  Filter
+		it *Iterator
+	}
+)
+
+// NewFilterExecutor returns a new Executor for filter.
+//
+// If f returns error, the iterator ends here.
+func NewFilterExecutor(f Filter, it *Iterator) Executor {
+	return &filterExecutor{
+		f:  f,
+		it: it,
+	}
+}
+
+func (s *filterExecutor) Execute() (*Iterator, error) {
+	var f func() (interface{}, error)
+	f = func() (interface{}, error) {
+		x, err := s.it.Next()
+		if err != nil {
+			return nil, err
+		}
+		v, err := s.f.Apply(x)
+		if err != nil {
+			// ends iterator
+			return nil, err
+		}
+		if !v {
+			// skip
+			return f()
+		}
+		return x, nil
+	}
+	return NewIterator(f)
+}
--- executor_test.go
+++ executor_test.go
@@ -35,3 +35,31 @@ func TestMapExecutor(t *testing.T) {
 		assert.Equal(t, circle.ErrEOI, err)
 	}
 }
+
+func TestFilterExecutor(t *testing.T) {
+	it, err := circle.NewIterator([]int{1, 2, 3, -1, 9})
+	assert.Nil(t, err)
+	f, err := circle.NewFilter(func(x int) (bool, error) {
+		if x < 0 {
+			return false, errors.New("negative")
+		}
+		return x&1 == 1, nil
+	})
+	assert.Nil(t, err)
+	exit, err := circle.NewFilterExecutor(f, it).Execute()
+	assert.Nil(t, err)
+	{
+		v, err := exit.Next()
+		assert.Nil(t, err)
+		assert.Equal(t, 1, v)
+	}
+	{
+		v, err := exit.Next()
+		assert.Nil(t, err)
+		assert.Equal(t, 3, v)
+	}
+	{
+		_, err := exit.Next()
+		assert.Equal(t, errors.New("negative"), err)
+	}
+}
