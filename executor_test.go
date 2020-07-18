6ab6715ae3b71f647826c1f945b5a79ccec24e0f
berquerant <sin@sin-pro.local>
2020-07-18 13:08:57 +0900

    Add test

--- executor_test.go
+++ executor_test.go
@@ -37,31 +37,48 @@ func ExampleNewMapExecutor() {
 }
 
 func TestMapExecutor(t *testing.T) {
-	it, err := circle.NewIterator([]int{-1, 0, 1})
-	assert.Nil(t, err)
-	f, err := circle.NewMapper(func(x int) (int, error) {
-		if x < 0 {
-			return 0, errors.New("negative")
+	t.Run("nil", func(t *testing.T) {
+		it, err := circle.NewIterator(nil)
+		assert.Nil(t, err)
+		f, err := circle.NewMapper(func(int) (int, error) {
+			return 0, nil
+		})
+		assert.Nil(t, err)
+		exit, err := circle.NewMapExecutor(f, it).Execute()
+		assert.Nil(t, err)
+		{
+			_, err := exit.Next()
+			assert.Equal(t, circle.ErrEOI, err)
 		}
-		return x + 10, nil
 	})
-	assert.Nil(t, err)
-	exit, err := circle.NewMapExecutor(f, it).Execute()
-	assert.Nil(t, err)
-	{
-		v, err := exit.Next()
+
+	t.Run("do", func(t *testing.T) {
+		it, err := circle.NewIterator([]int{-1, 0, 1})
 		assert.Nil(t, err)
-		assert.Equal(t, 10, v)
-	}
-	{
-		v, err := exit.Next()
+		f, err := circle.NewMapper(func(x int) (int, error) {
+			if x < 0 {
+				return 0, errors.New("negative")
+			}
+			return x + 10, nil
+		})
 		assert.Nil(t, err)
-		assert.Equal(t, 11, v)
-	}
-	{
-		_, err := exit.Next()
-		assert.Equal(t, circle.ErrEOI, err)
-	}
+		exit, err := circle.NewMapExecutor(f, it).Execute()
+		assert.Nil(t, err)
+		{
+			v, err := exit.Next()
+			assert.Nil(t, err)
+			assert.Equal(t, 10, v)
+		}
+		{
+			v, err := exit.Next()
+			assert.Nil(t, err)
+			assert.Equal(t, 11, v)
+		}
+		{
+			_, err := exit.Next()
+			assert.Equal(t, circle.ErrEOI, err)
+		}
+	})
 }
 
 func ExampleNewFilterExecutor() {
@@ -87,31 +104,48 @@ func ExampleNewFilterExecutor() {
 }
 
 func TestFilterExecutor(t *testing.T) {
-	it, err := circle.NewIterator([]int{1, 2, 3, -1, 9})
-	assert.Nil(t, err)
-	f, err := circle.NewFilter(func(x int) (bool, error) {
-		if x < 0 {
-			return false, errors.New("negative")
+	t.Run("nil", func(t *testing.T) {
+		it, err := circle.NewIterator(nil)
+		assert.Nil(t, err)
+		f, err := circle.NewFilter(func(int) (bool, error) {
+			return true, nil
+		})
+		assert.Nil(t, err)
+		exit, err := circle.NewFilterExecutor(f, it).Execute()
+		assert.Nil(t, err)
+		{
+			_, err := exit.Next()
+			assert.Equal(t, circle.ErrEOI, err)
 		}
-		return x&1 == 1, nil
 	})
-	assert.Nil(t, err)
-	exit, err := circle.NewFilterExecutor(f, it).Execute()
-	assert.Nil(t, err)
-	{
-		v, err := exit.Next()
+
+	t.Run("do", func(t *testing.T) {
+		it, err := circle.NewIterator([]int{1, 2, 3, -1, 9})
 		assert.Nil(t, err)
-		assert.Equal(t, 1, v)
-	}
-	{
-		v, err := exit.Next()
+		f, err := circle.NewFilter(func(x int) (bool, error) {
+			if x < 0 {
+				return false, errors.New("negative")
+			}
+			return x&1 == 1, nil
+		})
 		assert.Nil(t, err)
-		assert.Equal(t, 3, v)
-	}
-	{
-		_, err := exit.Next()
-		assert.Equal(t, errors.New("negative"), err)
-	}
+		exit, err := circle.NewFilterExecutor(f, it).Execute()
+		assert.Nil(t, err)
+		{
+			v, err := exit.Next()
+			assert.Nil(t, err)
+			assert.Equal(t, 1, v)
+		}
+		{
+			v, err := exit.Next()
+			assert.Nil(t, err)
+			assert.Equal(t, 3, v)
+		}
+		{
+			_, err := exit.Next()
+			assert.Equal(t, errors.New("negative"), err)
+		}
+	})
 }
 
 func ExampleNewAggregateExecutor_right() {
@@ -156,57 +190,122 @@ func TestAggregateExecutor(t *testing.T) {
 }
 
 func testLeftAggregateExecutor(t *testing.T) {
-	it, err := circle.NewIterator([]int{10, 9, 4})
-	assert.Nil(t, err)
-	f, err := circle.NewAggregator(func(x string, y int) (string, error) {
-		return fmt.Sprintf("%s+%d", x, y), nil
+	t.Run("nil", func(t *testing.T) {
+		it, err := circle.NewIterator(nil)
+		assert.Nil(t, err)
+		f, err := circle.NewAggregator(func(int, int) (int, error) {
+			return 0, nil
+		})
+		assert.Nil(t, err)
+		ex, err := circle.NewAggregateExecutor(f, it, 1, circle.WithAggregateExecutorType(circle.LAggregateExecutorType))
+		assert.Nil(t, err)
+		exit, err := ex.Execute()
+		assert.Nil(t, err)
+		{
+			v, err := exit.Next()
+			assert.Nil(t, err)
+			assert.Equal(t, 1, v)
+		}
+		{
+			_, err := exit.Next()
+			assert.Equal(t, circle.ErrEOI, err)
+		}
 	})
-	assert.Nil(t, err)
-	ex, err := circle.NewAggregateExecutor(f, it, "0")
-	assert.Nil(t, err)
-	exit, err := ex.Execute()
-	assert.Nil(t, err)
-	v, err := exit.Next()
-	assert.Nil(t, err)
-	assert.Equal(t, "0+10+9+4", v)
-	_, err = exit.Next()
-	assert.Equal(t, circle.ErrEOI, err)
+
+	t.Run("do", func(t *testing.T) {
+		it, err := circle.NewIterator([]int{10, 9, 4})
+		assert.Nil(t, err)
+		f, err := circle.NewAggregator(func(x string, y int) (string, error) {
+			return fmt.Sprintf("%s+%d", x, y), nil
+		})
+		assert.Nil(t, err)
+		ex, err := circle.NewAggregateExecutor(f, it, "0")
+		assert.Nil(t, err)
+		exit, err := ex.Execute()
+		assert.Nil(t, err)
+		v, err := exit.Next()
+		assert.Nil(t, err)
+		assert.Equal(t, "0+10+9+4", v)
+		_, err = exit.Next()
+		assert.Equal(t, circle.ErrEOI, err)
+	})
 }
 
 func testRightAggregateExecutor(t *testing.T) {
-	it, err := circle.NewIterator([]int{10, 9, 4})
-	assert.Nil(t, err)
-	f, err := circle.NewAggregator(func(x, y int) (int, error) {
-		return x - y, nil
+	t.Run("nil", func(t *testing.T) {
+		it, err := circle.NewIterator(nil)
+		assert.Nil(t, err)
+		f, err := circle.NewAggregator(func(int, int) (int, error) {
+			return 0, nil
+		})
+		assert.Nil(t, err)
+		ex, err := circle.NewAggregateExecutor(f, it, 1, circle.WithAggregateExecutorType(circle.RAggregateExecutorType))
+		assert.Nil(t, err)
+		exit, err := ex.Execute()
+		assert.Nil(t, err)
+		{
+			v, err := exit.Next()
+			assert.Nil(t, err)
+			assert.Equal(t, 1, v)
+		}
+		{
+			_, err := exit.Next()
+			assert.Equal(t, circle.ErrEOI, err)
+		}
 	})
-	assert.Nil(t, err)
-	ex, err := circle.NewAggregateExecutor(f, it, 0, circle.WithAggregateExecutorType(circle.RAggregateExecutorType))
-	assert.Nil(t, err)
-	exit, err := ex.Execute()
-	assert.Nil(t, err)
-	v, err := exit.Next()
-	assert.Nil(t, err)
-	assert.Equal(t, 5, v)
-	_, err = exit.Next()
-	assert.Equal(t, circle.ErrEOI, err)
+
+	t.Run("do", func(t *testing.T) {
+		it, err := circle.NewIterator([]int{10, 9, 4})
+		assert.Nil(t, err)
+		f, err := circle.NewAggregator(func(x, y int) (int, error) {
+			return x - y, nil
+		})
+		assert.Nil(t, err)
+		ex, err := circle.NewAggregateExecutor(f, it, 0, circle.WithAggregateExecutorType(circle.RAggregateExecutorType))
+		assert.Nil(t, err)
+		exit, err := ex.Execute()
+		assert.Nil(t, err)
+		v, err := exit.Next()
+		assert.Nil(t, err)
+		assert.Equal(t, 5, v)
+		_, err = exit.Next()
+		assert.Equal(t, circle.ErrEOI, err)
+	})
 }
 
 func TestCompareExecutor(t *testing.T) {
-	it, err := circle.NewIterator([]int{5, 2, 3, 1, 4})
-	assert.Nil(t, err)
-	f, err := circle.NewComparator(func(x, y int) (bool, error) {
-		return x < y, nil
+	t.Run("nil", func(t *testing.T) {
+		it, err := circle.NewIterator(nil)
+		assert.Nil(t, err)
+		f, err := circle.NewComparator(func(x, y int) (bool, error) {
+			return x < y, nil
+		})
+		assert.Nil(t, err)
+		exit, err := circle.NewCompareExecutor(f, it).Execute()
+		assert.Nil(t, err)
+		{
+			_, err := exit.Next()
+			assert.Equal(t, circle.ErrEOI, err)
+		}
 	})
-	assert.Nil(t, err)
-	exit, err := circle.NewCompareExecutor(f, it).Execute()
-	assert.Nil(t, err)
-	c := exit.Channel()
-	xs := []int{}
-	for v := range c.C() {
-		xs = append(xs, v.(int))
-	}
-	assert.Equal(t, "", cmp.Diff([]int{1, 2, 3, 4, 5}, xs))
-	assert.Nil(t, c.Err())
+
+	t.Run("do", func(t *testing.T) {
+		it, err := circle.NewIterator([]int{5, 2, 3, 1, 4})
+		assert.Nil(t, err)
+		f, err := circle.NewComparator(func(x, y int) (bool, error) {
+			return x < y, nil
+		})
+		assert.Nil(t, err)
+		exit, err := circle.NewCompareExecutor(f, it).Execute()
+		assert.Nil(t, err)
+		c := exit.Channel()
+		xs := []int{}
+		for v := range c.C() {
+			xs = append(xs, v.(int))
+		}
+		assert.Equal(t, "", cmp.Diff([]int{1, 2, 3, 4, 5}, xs))
+		assert.Nil(t, c.Err())
+	})
 }
 
 func ExampleNewFlatExecutor() {
@@ -227,8 +326,20 @@ func TestFlatExecutor(t *testing.T) {
 	for name, tc := range map[string]func(t *testing.T){
 		"noop":    testFlatExecutorNoop,
 		"onestep": testFlatExecutorFlatOneStep,
+		"nil":     testFlatExecutorNil,
 	} {
 		t.Run(name, tc)
+	}
+}
+
+func testFlatExecutorNil(t *testing.T) {
+	it, err := circle.NewIterator(nil)
+	assert.Nil(t, err)
+	exit, err := circle.NewFlatExecutor(it).Execute()
+	assert.Nil(t, err)
+	{
+		_, err := exit.Next()
+		assert.Equal(t, circle.ErrEOI, err)
 	}
 }
 
